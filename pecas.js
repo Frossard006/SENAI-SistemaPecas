d34f06280e142fd4942d114b651b7b94281953e1
Matheus Frossard <matheus.frossard@hotmail.com>
2022-01-24 18:30:39 -0300

    Finalização do sistema de peças

--- pecas.js
+++ pecas.js
@@ -1,9 +1,27 @@
-var n1 = 7
-var n2 = 8
-var n3 = 6
-var media = (n1+n2+n3)/3
-if (media >= 7){
-    console.log("Aprovado", media)
-}else{
-    console.log("Reprovado", media)
+let pesoPeca  = 100
+
+if(pesoPeca > 100) {
+    console.log("Peso maior que 100g, podemos cadastrar a peça.")
+} else { 
+    console.log("Peso insuficiente, impossível cadastrar a peça.")
+}
+
+
+let numeroPecas = 10
+
+if (numeroPecas < 10) {
+    console.log ("Numero de peças menor que 10, podemos incluir na caixa.")
+} else {
+    console.log ("Numero de peças maior que 10, impossível incluir na caixa.")
+}
+
+
+let nomePeca = "Disco de freio"
+
+console.log("O comprimento do nome da peça é:", nomePeca.length)
+
+if (nomePeca.length < 3) {
+    console.log ("Nome da peça muito curto, impossível cadastrar.")
+} else {
+    console.log ("Nome da peça adequado, podemos cadastrar")
 }
